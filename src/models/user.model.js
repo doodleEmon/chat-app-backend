1ed2fc5263e16fb9d729a4eaceb734f7a2095359
Emon <150001421+doodleEmon@users.noreply.github.com>
2025-09-15 14:54:15 +0600

    user schema created done

---
+++ src/models/user.model.js
@@ -0,0 +1,27 @@
+import { Model, Schema } from "mongoose";
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+    },
+    profilePic: {
+        type: String,
+        default: ""
+    }
+}, {
+    timestamps: true
+});
+
+const User = new Model('User', userSchema);
+export default User;
