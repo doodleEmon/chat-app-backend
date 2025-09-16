5d8a29ae0df603ba5b29e3066bfdda8159f82738
Emon <150001421+doodleEmon@users.noreply.github.com>
2025-09-16 22:38:23 +0600

    profile update functionality done

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -105,5 +105,25 @@ export const logout = (req, res) => {
 }
 
 export const updateProfile = async (req, res) => {
-    
+    try {
+        const { profilePic } = req.body;
+        const userId = req.user._id;
+
+        if (!profilePic) {
+            res.status(400).json({ message: "Profile picture is required" });
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+
+        const updatedUser = await User.findByIdAndUpdate(
+            userId,
+            { profilePic: uploadResponse.secure_url },
+            { new: true }
+        )
+
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        console.log("Error from updateProfile controller:", error.message);
+        res.status(500).json({ message: "Internal server error!" });
+    }
 }
