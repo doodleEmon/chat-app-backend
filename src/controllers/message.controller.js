4a9acdbed4fbff12189c43f816da0a1eb236e51b
Emon <150001421+doodleEmon@users.noreply.github.com>
2025-09-17 02:10:12 +0600

    sendMessage function done

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -30,3 +30,34 @@ export const getMessages = async (req, res) => {
         res.status(500).json({ message: "Internal server error" });
     }
 }
+
+export const sendMessages = async (req, res) => {
+    try {
+        const { id: receiverId } = req.params;
+        const myId = req.user._id;
+        const { text, image } = req.body;
+
+        // if image provided, then have to upload into cloudinary and take a image url
+        let imageUrl;
+        if (image) {
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+            senderId: myId,
+            receiverId,
+            text,
+            image: imageUrl
+        })
+
+        // todo: real time implementation: socket.io
+
+        await newMessage.save();
+
+        res.status(201).json(newMessage);
+    } catch (error) {
+        console.log("Error of sendMessages controller:", error.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
--- src/routes/message.route.js
+++ src/routes/message.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { protectRoute } from "../middlewares/auth.middleware.js";
-import { getMessages, getUsers } from "../controllers/message.controller.js";
+import { getMessages, getUsers, sendMessages } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsers);
 router.get("/:id", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, sendMessages);
 
 export default router;
