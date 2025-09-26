c326c3df972120c6a9e5035a5b17a590ae8dea2a
Emon <150001421+doodleEmon@users.noreply.github.com>
2025-09-26 12:58:56 +0600

    index.js updated

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const databaseConnection = connectToDb();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:3000/',
+    origin: 'http://localhost:3000',
     credentials: true
 }))
 
