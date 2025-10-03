1d4aefd0ceed1402e929986bb1ca3c47cdd989e5
Emon <150001421+doodleEmon@users.noreply.github.com>
2025-10-03 20:07:43 +0600

    index.js updated

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ const app = express();
 const port = process.env.PORT;
 const databaseConnection = connectToDb();
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(cors({
     origin: 'http://localhost:3000',
