21456ae985f869bad6058fb8c5d8ba6cfb36a59b
Emon <150001421+doodleEmon@users.noreply.github.com>
2025-10-22 23:04:12 +0600

    message.controller.js updated

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -2,18 +2,6 @@ import cloudinary from "../lib/cloudinary.js";
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 
-// export const getUsers = async (req, res) => {
-//     try {
-//         const myId = req.user._id;
-//         const otherUsers = await User.find({ _id: { $ne: myId } }).select("-password");
-
-//         res.status(200).json(otherUsers);
-//     } catch (error) {
-//         console.log("Error of getUsers controller:", error.message);
-//         res.status(500).json({ message: "Internal server error" });
-//     }
-// }
-
 export const getUsers = async (req, res) => {
     try {
         const myId = req.user._id;
