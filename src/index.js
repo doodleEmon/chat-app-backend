216b6a1c745159554d11c83ee90b4dcb60d2f80f
Emon <150001421+doodleEmon@users.noreply.github.com>
2025-11-10 14:51:25 +0600

    index.js updated

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://chat-app-frontend-ten-kappa.vercel.app',
     credentials: true
 }))
 
